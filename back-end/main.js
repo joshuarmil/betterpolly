55c38a07492f318b47b0719522c37e1d450d8482
ptravers <traverspw@gmail.com>
2018-08-02 16:37:01 -0400

    Project bones

--- back-end/main.js
+++ back-end/main.js
@@ -1,12 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+	fs.readFile('views/home.html', function(err, data) {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          res.write(data);
+          res.end();
+	});
 });
 
 server.listen(port, hostname, () => {
--- selenium-tests/test_homepage_exists.js
+++ selenium-tests/test_homepage_exists.js
@@ -1,11 +1,30 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
-var driver = new Builder().forBrowser('firefox').build();
+const Assert = require('assert');
 
-driver.get('http://localhost:8080/');
+const host = "localhost";
+const port = 3000;
+const authority = host + ":" + port;
+const uri = "http://" + authority;
 
-driver.findElement(By.name('navbar'));
+var runTests = 
+  (async function (browser) {
+    let driver = await new Builder().forBrowser(browser).build();
+    let expectedMessage = "Welcome to our Secret Santa or Polyanna site! You can now easily send emails to all your friends and start a secret santa!";
+    try {
+      await driver.get(uri + "/");
+      let element = await driver
+		    .findElement(By.name('about'))
+		    .getText()
+		    .then(function(text) {
+                       let testMessage = "Is the about page present and does it contain expecected text\n" + expectedMessage + "actual\n" + text;
+                       Assert(expectedMessage == text, testMessage);
+		    });
+    } catch(error) {
+      Assert.fail(error.message);
+    } finally {
+      await driver.quit();
+    }
+  });
 
-driver.findElement(By.name('info'));
-
-driver.quit();
-
+runTests('firefox')
+runTests('chrome')
