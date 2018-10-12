0cd5a79e5a7b724e663ecb08c5e73e987605ff13
joshuarmil <tronnytsunami@gmail.com>
2018-10-11 22:25:14 -0400

    Add dependencies and basic routing

--- back-end/main.js
+++ back-end/main.js
@@ -1,6 +1,18 @@
 const http = require('http');
+const url = require('url');
 const fs = require('fs');
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+const statusRouter = require('./status.js');
+const path = require('path');
 
+app.use(logger('dev'));
 
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/views/home.html'));
 });
 
+app.use('/status', statusRouter);
+
+module.exports = app;
