4eb5908e1b3a0838252d60e8b86f677cb5c81f11
joshuarmil <31965595+joshuarmil@users.noreply.github.com>
2019-07-17 13:58:27 -0400

    Receive post requests, closes #10

--- back-end/main.js
+++ back-end/main.js
@@ -1,19 +1,27 @@
-const express = require('express');
-const logger = require('morgan');
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/views/home.html'));
+app.get("/", function(req, res) {
+	res.sendFile(path.join(__dirname + "/views/test.html"));
 });
 
-app.get('/status', function(req, res) {
+app.get("/status", function(req, res) {
 	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.write('UP');
+	res.setHeader("Content-Type", "text/plain");
+	res.write("UP");
 	res.end();
+});
+
+app.post("/post", function(req, res) {
+	console.log(req.body);
+	res.end("post");
 });
 
 module.exports = app;
