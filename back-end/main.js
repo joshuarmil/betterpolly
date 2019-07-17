3a7352e3725412ec63fde76d6e113be486f95159
joshuarmil <31965595+joshuarmil@users.noreply.github.com>
2019-07-17 14:49:11 -0400

    Add emailing functionality, close #11

--- back-end/main.js
+++ back-end/main.js
@@ -3,6 +3,22 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAILADDRESS,
+		pass: process.env.EMAILPASS
+	}
+});
+
+const mailOptions = {
+	from: process.env.EMAILADDRESS,
+	to: process.env.RECIPIENT,
+	subject: 'Polly Anna Time!',
+	html: '<p>Hi, how are you?</p>'
+};
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,6 +37,10 @@ app.get('/status', function(req, res) {
 
 app.post('/post', function(req, res) {
 	console.log(req.body);
+	transporter.sendMail(mailOptions, function(err, info) {
+		if (err) console.log(err);
+		else console.log(info);
+	});
 	res.end('post');
 });
 
