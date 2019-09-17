d129e038221f5a703f2b04d63c177899db9f70bb
joshuarmil <31965595+joshuarmil@users.noreply.github.com>
2019-09-16 23:41:36 -0400

    Test that every user receives an email

--- back-end/tests/routes.test.js
+++ back-end/tests/routes.test.js
@@ -1,8 +1,8 @@
-const expect = require('chai').expect;
-const { pollyannerize } = require('../pollyannerize');
+const expect = require("chai").expect;
+const { pollyannerize } = require("../pollyannerize");
 
-describe('Pollyannerize Route', function() {
-	it('Should error out if no master email is provided', function() {
+describe("Pollyannerize Route", function() {
+	it("Should error out if no master email is provided", function() {
 		let req = {
 			body: {
 				users: {},
@@ -11,28 +11,72 @@ describe('Pollyannerize Route', function() {
 		};
 
 		let res = {
-			sendCalledWith: ' ',
+			sendCalledWith: " ",
 			send: function(arg) {
 				this.sendCalledWith = arg;
 			}
 		};
 		pollyannerize(null)(req, res);
-		expect(res.sendCalledWith).to.contain('error');
+		expect(res.sendCalledWith).to.contain("error");
 	});
-	it('Should send master email', function() {
+	it("Should error if there are duplicate pollyannas", function() {
 		let req = {
 			body: {
-				users: [],
+				users: [
+					{
+						name: "userName1",
+						family: "userFamily1",
+						email: "userEmail1"
+					},
+					{
+						name: "userName2",
+						family: "userFamily2",
+						email: "userEmail2"
+					},
+					{
+						name: "userName2",
+						family: "userFamily2",
+						email: "userEmail2"
+					}
+				],
 				master: {
-					name: 'testName',
-					family: 'testFamily',
-					email: 'testEmail'
+					name: "masterName",
+					family: "masterFamily",
+					email: "masterEmail"
 				}
 			}
 		};
 
 		let res = {
-			sendCalledWith: ' ',
+			sendCalledWith: " ",
+			send: function(arg) {
+				this.sendCalledWith = arg;
+			}
+		};
+
+		let sentEmail = null;
+
+		const sendEmail = email => {
+			sentEmail = [];
+		};
+
+		pollyannerize(sendEmail)(req, res);
+		expect(res.sendCalledWith).to.contain("duplicate");
+	});
+	it("Should send master email", function() {
+		let req = {
+			body: {
+				users: [],
+				master: {
+					name: "testName",
+					family: "testFamily",
+					email: "testEmail"
+				}
+			}
+		};
+
+		let res = {
+			sendCalledWith: " ",
 			send: function(arg) {
 				this.sendCalledWith = arg;
 			}
@@ -47,26 +91,26 @@ describe('Pollyannerize Route', function() {
 		pollyannerize(sendEmail)(req, res);
 		expect(sentEmail.to).to.contain(req.body.master.email);
 	});
-	it('Should send each user an email', function() {
+	it("Should send a user an email", function() {
 		let req = {
 			body: {
 				users: [
 					{
-						name: 'userName',
-						family: 'userFamily',
-						email: 'userEmail'
+						name: "userName",
+						family: "userFamily",
+						email: "userEmail"
 					}
 				],
 				master: {
-					name: 'masterName',
-					family: 'masterFamily',
-					email: 'masterEmail'
+					name: "masterName",
+					family: "masterFamily",
+					email: "masterEmail"
 				}
 			}
 		};
 
 		let res = {
-			sendCalledWith: ' ',
+			sendCalledWith: " ",
 			send: function(arg) {
 				this.sendCalledWith = arg;
 			}
@@ -81,48 +125,48 @@ describe('Pollyannerize Route', function() {
 		pollyannerize(sendEmail)(req, res);
 		expect(sentEmail.to).to.contain(req.body.users[0].email);
 	});
-	it('Should error if there are duplicate pollyannas', function() {
+	it("Should send every user an email", function() {
 		let req = {
 			body: {
 				users: [
 					{
-						name: 'userName1',
-						family: 'userFamily1',
-						email: 'userEmail1'
+						name: "userName1",
+						family: "userFamily1",
+						email: "userEmail1"
 					},
 					{
-						name: 'userName2',
-						family: 'userFamily2',
-						email: 'userEmail2'
+						name: "userName2",
+						family: "userFamily2",
+						email: "userEmail2"
 					},
 					{
-						name: 'userName2',
-						family: 'userFamily2',
-						email: 'userEmail2'
+						name: "userName3",
+						family: "userFamily3",
+						email: "userEmail3"
 					}
 				],
 				master: {
-					name: 'masterName',
-					family: 'masterFamily',
-					email: 'masterEmail'
+					name: "masterName",
+					family: "masterFamily",
+					email: "masterEmail"
 				}
 			}
 		};
 
 		let res = {
-			sendCalledWith: ' ',
+			sendCalledWith: " ",
 			send: function(arg) {
 				this.sendCalledWith = arg;
 			}
 		};
 
-		let sentEmail = null;
+		let sentEmail = [];
 
 		const sendEmail = email => {
-			sentEmail = email;
+			sentEmail.push(email.to);
 		};
 
 		pollyannerize(sendEmail)(req, res);
-		expect(res.sendCalledWith).to.contain('duplicate');
+		expect(sentEmail).to.have.lengthOf(req.body.users.length + 1); // Add 1 for Master
 	});
 });
